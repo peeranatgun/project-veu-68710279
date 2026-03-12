b4be13184bf313a3c718b92a108aeb8657ffa64a
peeranatgun <68710279@chonburi.spu.ac.th>
2026-03-11 18:48:36 -0700

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <br>
+    <h1>คณะเทคโนโลยีสารสนเทศ</h1>
+    <h1>Peeranatgun</h1>
+  </div>
+</template>
---
+++ src/views/Contact.vue
@@ -0,0 +1,22 @@
+<template lang="">
+    <br>
+  <div class="container">
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
+
+    <div>
+      <h1>Show contact</h1>
+      <h3>Peeranat sawatdichai</h3>
+      <p>welcome vue</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+h3 {
+  color: rgb(43, 100, 224);
+}
+</style>
